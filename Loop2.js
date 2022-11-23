593979c1b69255ae9dc2e1c3dfceeed81713b4de
Wesley Holt <108308740+WesHoltHD@users.noreply.github.com>
2022-11-22 18:52:22 -0800

    Add files via upload

---
+++ Loop2.js
@@ -0,0 +1,7 @@
+// Decreasing Multiples of 3
+// Using a loop write code that will console.log all of the values that are evenly divisible by 3 from 100 down to 0.
+
+for (var i=0 ; i<101; i++){
+    if (i/3 ==0);
+    console.log(i);
+}
---
+++ Loop5.js
@@ -0,0 +1,5 @@
+// Factorial
+// Write code that will multiply all of the values from 1-12 onto some variable product and at the end console.log the result 1 * 2 * 3 * ... * 10 * 11 * 12. We should get back 479001600 at the end.
+
+for (var i=1 ; i<13; i++;)
+console.log();
